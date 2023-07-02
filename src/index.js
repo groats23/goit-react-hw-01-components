3828391afcba40ebeeb83d8f0ee8dcbb517b3ab7
groats23 <121118901+groats23@users.noreply.github.com>
2023-07-02 20:55:46 +0200

    alles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import reportWebVitals from 'reportWebVitals';
 import { App } from 'components/App';
-import './index.css';
+import 'index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,3 +11,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 );
 
 reportWebVitals();
+
+// console.log(reportWebVitals);
